a6cf87e7b921cf734a5de9afc0deb43d2dce8023
harolditoc <harold.chambilla@usil.pe>
2023-04-02 21:04:08 -0500

    cambio

--- assets/app.js
+++ assets/app.js
@@ -7,6 +7,10 @@
 
 // any CSS you import will output into a single css file (app.css in this case)
 import './styles/app.css';
-
 // start the Stimulus application
 import './bootstrap';
+const $ = require('jquery');
+require('bootstrap');
+$(document).ready(function() {
+    $('[data-toggle="popover"]').popover();
+});
